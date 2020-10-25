be9d7a7d3838a02ba77519a32abe3d5eda79b995
cuncinc <cuncinc@foxmail.com>
2020-10-25 14:32:10 +0800

    merge 把PublicFragment改为SquareFragment

--- app/src/main/java/kilig/ink/yxy/activity/LoginActivity.java
+++ app/src/main/java/kilig/ink/yxy/activity/LoginActivity.java
@@ -3,13 +3,16 @@ package kilig.ink.yxy.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -39,6 +42,8 @@ import okhttp3.ResponseBody;
 public class LoginActivity extends AppCompatActivity {
     private CatLoadingView mView;
     private static final String TAG = "MainActivity";
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,10 +53,26 @@ public class LoginActivity extends AppCompatActivity {
         TextInputEditText username = findViewById(R.id.yxyUsername);
         //密码
         TextInputEditText password = findViewById(R.id.yxyPassword);
+        //登录
         Button login = findViewById(R.id.login);
+        //注册
         Button rigister = findViewById(R.id.btn_rigister);
+        //记住密码
+        CheckBox rememberpsw= findViewById(R.id.ckb_rememberpsw);
+        //获取SharedPreferences对象
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isRemember = pref.getBoolean("remember_password", false);
+        if (isRemember) {
+            // 将账号和密码都设置到文本框中
+            String tUsername = pref.getString("account", "");
+            String tPassword = pref.getString("password", "");
+            username.setText(tUsername);
+            password.setText(tPassword);
+            rememberpsw.setChecked(true);
+        }
 
         Log.d(TAG, "onCreate: 999");
+        //登录逻辑
         login.setOnClickListener(v->{
 
             Map<String,String> loginInfo=new HashMap<>();
@@ -72,22 +93,9 @@ public class LoginActivity extends AppCompatActivity {
                 OkhttpUtils.post("yxyUser/login", info, new Callback() {
                     @Override
                     public void onFailure(@NotNull Call call, @NotNull IOException e) {
-
-                        try {
-                            Thread.sleep(1000);
-
-                        } catch (InterruptedException q) {
-                            q.printStackTrace();
-                        }
-                        runOnUiThread(()->{
-
-                            FancyToast.makeText(LoginActivity.this,"由于未知的原因，登录失败，请检查一下您的网络是否有问题?",
-                                    FancyToast.LENGTH_SHORT,
-                                    FancyToast.WARNING,
-                                    false).show();
-                            mView.onDestroyView();
-                        });
-
+                        Log.d(TAG, "onFailure: "+e.getMessage());
+                        mView.onDestroyView();
+                        Log.d(TAG, "onFailure: 666");
 
                     }
 
@@ -98,15 +106,11 @@ public class LoginActivity extends AppCompatActivity {
                         String s = responseBody.string();
                         ResponeObject object = gson.fromJson(s, ResponeObject.class);
                         //关闭窗口
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+                        mView.onDestroyView();
+
                         //判断状态码 200 ok xxx 不ok
                         if(object.getCode() == null || !object.getCode().equals("200")){
                             runOnUiThread(()->{
-                                mView.onDestroyView();
                                 FancyToast.makeText(LoginActivity.this,object.getMessage(),
                                         FancyToast.LENGTH_SHORT,
                                         FancyToast.ERROR,
@@ -114,18 +118,13 @@ public class LoginActivity extends AppCompatActivity {
                             });
                         }else {
                             runOnUiThread(()->{
-                                mView.onDestroyView();
                                 FancyToast.makeText(LoginActivity.this,object.getMessage(),
                                         FancyToast.LENGTH_SHORT,
                                         FancyToast.SUCCESS,
                                         false).show();
                             });
                             OkhttpUtils.setToken((String) object.getData()); //传递token
-
-
                             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                            //禁止后退回到登录界面
-                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
                             startActivity(intent);
 
 
@@ -143,6 +142,7 @@ public class LoginActivity extends AppCompatActivity {
 
         });
 
+        //跳转到注册页面
         rigister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
