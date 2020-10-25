b7051ae7c42d10c6366f5b49d5acd3fbd80c9007
mengsuliang <2504773400@qq.com>
2020-10-25 13:53:13 +0800

    fix the bug

--- app/src/main/java/kilig/ink/yxy/activity/LoginActivity.java
+++ app/src/main/java/kilig/ink/yxy/activity/LoginActivity.java
@@ -3,13 +3,16 @@ package kilig.ink.yxy.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -39,6 +42,8 @@ import okhttp3.ResponseBody;
 public class LoginActivity extends AppCompatActivity {
     private CatLoadingView mView;
     private static final String TAG = "MainActivity";
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,10 +53,26 @@ public class LoginActivity extends AppCompatActivity {
         TextInputEditText username = findViewById(R.id.yxyUsername);
         //密码
         TextInputEditText password = findViewById(R.id.yxyPassword);
+        //登录
         Button login = findViewById(R.id.login);
+        //注册
         Button rigister = findViewById(R.id.btn_rigister);
+        //记住密码
+        CheckBox rememberpsw= findViewById(R.id.ckb_rememberpsw);
+        //获取SharedPreferences对象
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isRemember = pref.getBoolean("remember_password", false);
+        if (isRemember) {
+            // 将账号和密码都设置到文本框中
+            String tUsername = pref.getString("account", "");
+            String tPassword = pref.getString("password", "");
+            username.setText(tUsername);
+            password.setText(tPassword);
+            rememberpsw.setChecked(true);
+        }
 
         Log.d(TAG, "onCreate: 999");
+        //登录逻辑
         login.setOnClickListener(v->{
 
             Map<String,String> loginInfo=new HashMap<>();
@@ -121,6 +142,7 @@ public class LoginActivity extends AppCompatActivity {
 
         });
 
+        //跳转到注册页面
         rigister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
