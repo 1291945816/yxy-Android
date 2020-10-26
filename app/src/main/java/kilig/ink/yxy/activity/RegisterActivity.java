021cc7fcd34f2cf0db45d10ecdd630a6b3149002
mengsuliang <2504773400@qq.com>
2020-10-26 22:56:54 +0800

    改名注册活动

--- app/src/main/java/kilig/ink/yxy/activity/RegisterActivity.java
+++ app/src/main/java/kilig/ink/yxy/activity/RegisterActivity.java
@@ -4,22 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.gson.Gson;
 
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import kilig.ink.yxy.R;
 import kilig.ink.yxy.utils.OkhttpUtils;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "RegisterAcitivt";
     private TextInputEditText rgstUsername;
     private TextInputEditText rgstPassword;
     private TextInputEditText check_rgstPassword;
@@ -58,6 +70,29 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.click_rgst:
+                Map<String,String> registerInfo = new HashMap<>();
+                registerInfo.put("username",rgstUsername.getText().toString());
+                registerInfo.put("password",rgstPassword.getText().toString());
+
+                Gson gson = new Gson();
+                String rginfo = gson.toJson(registerInfo);
+                Log.d(TAG,rginfo);
+
+                try {
+                    OkhttpUtils.post("yxyUser/register", rginfo, new Callback() {
+                        @Override
+                        public void onFailure(@NotNull Call call, @NotNull IOException e) {
+
+                        }
+
+                        @Override
+                        public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
                 break;
             case R.id.btn_backup:
