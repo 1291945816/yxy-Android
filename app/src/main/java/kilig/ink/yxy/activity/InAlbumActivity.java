f5b35268f85d38d95b79e0c37cf963dfb5ec80ea
witie41 <1442348098@qq.com>
2020-12-09 15:56:50 +0800

    完成从相册选取图片

--- app/src/main/java/kilig/ink/yxy/activity/InAlbumActivity.java
+++ app/src/main/java/kilig/ink/yxy/activity/InAlbumActivity.java
@@ -19,22 +19,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
+import androidx.recyclerview.widget.SimpleItemAnimator;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.hitomi.tilibrary.transfer.TransferConfig;
 import com.hitomi.tilibrary.transfer.Transferee;
+import com.luck.picture.lib.PictureSelector;
+import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.config.PictureMimeType;
+import com.luck.picture.lib.entity.LocalMedia;
+import com.shashank.sony.fancytoastlib.FancyToast;
+import com.shashank.sony.fancytoastlib.FancyToast;
 import com.vansz.glideimageloader.GlideImageLoader;
 import com.vansz.universalimageloader.UniversalImageLoader;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import kilig.ink.yxy.R;
 import kilig.ink.yxy.entity.AblumItem;
@@ -44,7 +57,9 @@ import kilig.ink.yxy.entity.PhotoItem;
 import kilig.ink.yxy.entity.ResponeObject;
 import kilig.ink.yxy.entity.SpacesItemDecoration;
 import kilig.ink.yxy.source.AlbumsAdapter;
+import kilig.ink.yxy.source.GlideEngine;
 import kilig.ink.yxy.source.InAlbumAdapter;
+import kilig.ink.yxy.utils.FileProgressRequestBody;
 import kilig.ink.yxy.utils.OkhttpUtils;
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -75,6 +90,7 @@ public class InAlbumActivity extends AppCompatActivity {
         topTitle.setText(ablumItem.getAblumName());
         InitData();
         config();
+
        recyclerView = (RecyclerView)findViewById(R.id.inalbum_pict);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
@@ -88,6 +104,20 @@ public class InAlbumActivity extends AppCompatActivity {
         });
 
         recyclerView.setAdapter(adapter);
+        Activity activity = this;
+        // 添加图片按钮
+        FloatingActionButton floatingActionButton = findViewById(R.id.floating_action_button_in_album);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PictureSelector.create(activity)
+                        .openGallery(PictureMimeType.ofImage())
+                        .loadImageEngine(GlideEngine.createGlideEngine()) // 请参考Demo GlideEngine.java
+                        .forResult(PictureConfig.CHOOSE_REQUEST);
+            }
+        });
+
+
 
 
 
@@ -102,6 +132,83 @@ public class InAlbumActivity extends AppCompatActivity {
         tv_return.setOnClickListener(e -> {
             finish();
         });
+    }
+
+    private void deleteItemPicture(int pos, InalbumPicture inalbumPicture) {
+        try {
+            OkhttpUtils.post("ablum/deletePicture/" + inalbumPicture.getId(), "", new Callback() {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                    runOnUiThread(()->{
+                        FancyToast.makeText(getApplicationContext(),"出现异常，删除失败",FancyToast.SUCCESS,FancyToast.LENGTH_SHORT,false).show();
+                    });
+                }
+
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                    String json = response.body().string();
+                    ResponeObject responeObject = new Gson().fromJson(json, ResponeObject.class);
+                    if (responeObject != null && responeObject.getCode().equals("200")){
+                        runOnUiThread(()->{
+                            adapter.removeItem(pos);
+                            FancyToast.makeText(getApplicationContext(),responeObject.getMessage(),FancyToast.SUCCESS,FancyToast.LENGTH_SHORT,false).show();
+                        });
+                    }else {
+                        runOnUiThread(()->{
+                            FancyToast.makeText(getApplicationContext(),"出现异常，删除失败",FancyToast.SUCCESS,FancyToast.LENGTH_SHORT,false).show();
+                        });
+                    }
+
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case PictureConfig.CHOOSE_REQUEST:
+                    // todo zyb 2 hps:写一下图片上传接口
+                    // 获取到所有的选择结果
+                    // 其中localMedia.getPath()是照片在设备中的绝对地址
+                    // 下面有个imageToBase64方法，是把图片路径转化为byte[](应该能用，还没测试)
+                    List<LocalMedia> selectList = PictureSelector.obtainMultipleResult(data);
+                    for (LocalMedia localMedia: selectList) {
+                        Map<String,String> Image_info = new HashMap<String,String>();
+                        Image_info.put("publishVisiable","0");
+                        Image_info.put("pictureInfo",localMedia.getFileName());
+                        Image_info.put("ablumId",String.valueOf(ablumItem.getAblumId()));
+                        Image_info.put("pictureName",localMedia.getFileName());
+                        Image_info.put("file", Arrays.toString(imageToBase64(localMedia.getPath())));
+                        // 下面的是我测试的代码，可以删了
+                        try {
+                            OkhttpUtils.postWithBody("picture/uploadPicture", Image_info,listener, new Callback() {
+                                @Override
+                                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                                    InitData();
+                                    Log.e("UploadImage", response.toString() );
+                                }
+
+                                @Override
+                                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                                }
+                            });
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
     void InitData() {
@@ -151,4 +258,47 @@ public class InAlbumActivity extends AppCompatActivity {
                  .setImageLoader(GlideImageLoader.with(getApplicationContext()))
                 .bindRecyclerView(recyclerView,R.id.item_photo);
     }
+
+
+    private void changePublish(int pos,InalbumPicture picture){
+        Map<String,String> map=new HashMap<>();
+        map.put("pictureId",picture.getId());
+        map.put("publish",String.valueOf(!picture.isPublish()));
+        String toJson = new Gson().toJson(map);
+        try {
+            OkhttpUtils.post("ablum/changePublishStatus", toJson, new Callback() {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                    runOnUiThread(()->{
+                        FancyToast.makeText(getApplicationContext(),e.getMessage(),FancyToast.ERROR,FancyToast.LENGTH_SHORT,false).show();
+                    });
+
+
+                }
+
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                    String responseString = response.body().string();
+                    ResponeObject responeObject = new Gson().fromJson(responseString, ResponeObject.class);
+                    if (responeObject== null || !responeObject.getCode().equals("200")){
+                        runOnUiThread(()->{
+                            FancyToast.makeText(getApplicationContext(),"更新失败，请重试",FancyToast.ERROR,FancyToast.LENGTH_SHORT,false).show();
+                        });
+                    }else{
+                        runOnUiThread(()->{
+                            picture.setPublish(!picture.isPublish());
+
+                            adapter.notifyItemChanged(pos);
+                            FancyToast.makeText(getApplicationContext(),"更新发布状态成功",FancyToast.SUCCESS,FancyToast.LENGTH_SHORT,false).show();
+                        });
+                    }
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
